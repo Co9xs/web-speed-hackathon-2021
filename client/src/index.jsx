4ee64a63caf4e2edea5220bdf68dfa9bed1e77a3
Co9xs <ryo.fjsm0203@icloud.com>
2021-12-05 11:13:24 +0900

    client: scriptの実行タイミングをdeferに変更

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import { AppContainer } from './containers/AppContainer';
-
-window.addEventListener('load', () => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <AppContainer />
-    </BrowserRouter>,
-    document.getElementById('app'),
-  );
-});
+ReactDOM.render(
+  <BrowserRouter>
+    <AppContainer />
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
