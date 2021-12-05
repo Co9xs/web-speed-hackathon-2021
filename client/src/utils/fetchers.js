0710d8497f5610804f28c4b81970bf47780077c6
Co9xs <ryo.fjsm0203@icloud.com>
2021-12-05 18:14:15 +0900

    client: jQueryをfetchAPIで置き換え

--- client/src/utils/fetchers.js
+++ client/src/utils/fetchers.js
@@ -5,14 +5,13 @@ import { gzip } from 'pako';
  * @returns {Promise<ArrayBuffer>}
  */
 async function fetchBinary(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'binary',
+  const res = await fetch(url, {
     method: 'GET',
-    responseType: 'arraybuffer',
-    url,
+    headers: {
+      'Accept': 'application/octet-stream'
+    }
   });
-  return result;
+  return await res.arrayBuffer();
 }
 
 /**
@@ -21,13 +20,13 @@ async function fetchBinary(url) {
  * @returns {Promise<T>}
  */
 async function fetchJSON(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'json',
+  const res = await fetch(url, {
     method: 'GET',
-    url,
-  });
-  return result;
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  return await res.json();
 }
 
 /**
@@ -37,18 +36,15 @@ async function fetchJSON(url) {
  * @returns {Promise<T>}
  */
 async function sendFile(url, file) {
-  const result = await $.ajax({
-    async: false,
-    data: file,
-    dataType: 'json',
+  const res = await fetch(url, {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/octet-stream',
+      'Accept': 'application/json'
     },
-    method: 'POST',
-    processData: false,
-    url,
+    body: file,
   });
-  return result;
+  return await res.json();
 }
 
 /**
@@ -62,19 +58,16 @@ async function sendJSON(url, data) {
   const uint8Array = new TextEncoder().encode(jsonString);
   const compressed = gzip(uint8Array);
 
-  const result = await $.ajax({
-    async: false,
-    data: compressed,
-    dataType: 'json',
+  const res = await fetch(url, {
+    method: 'POST',
     headers: {
       'Content-Encoding': 'gzip',
       'Content-Type': 'application/json',
+      'Accept': 'application/json'
     },
-    method: 'POST',
-    processData: false,
-    url,
-  });
-  return result;
+    body: compressed
+  })
+  return await res.json();
 }
 
 export { fetchBinary, fetchJSON, sendFile, sendJSON };
